b31b1760c608f3bdb9521b103addfc5cda6a78e9
kaleem362 <matrixdev19@gmail.com>
2026-01-04 00:42:32 +0500

    spinning the logo enabled

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -7,25 +7,31 @@ const Navbar = () => {
   return (
     <nav className="p-4 flex items-center justify-around">
       <div className="img-logo h-14 w-14">
-        <img src={ASSETS.logo} alt="" />
+        <img
+          src={ASSETS.logo}
+          alt=""
+          className="animate-[spin_6s_linear_infinite]"
+        />
       </div>
 
       <ul className="nav-items flex gap-2 xs:gap-1 md:gap-4 lg:gap-6 xl:gap-8 items-center justify-around">
-  {["Home","Our Work","Projects","Contact","About","Blog"].map((item)=>(
-    <li
-      key={item}
-      className="relative group text-indigo-400 cursor-pointer 
+        {["Home", "Our Work", "Projects", "Contact", "About", "Blog"].map(
+          (item) => (
+            <li
+              key={item}
+              className="relative group text-indigo-400 cursor-pointer 
                  xs:text-xs md:text-lg lg:text-xl xl:text-lg hover:text-indigo-500"
-    >
-      {item}
-      <span
-        className="absolute left-0 -bottom-1 h-0.5 w-0 
+            >
+              {item}
+              <span
+                className="absolute left-0 -bottom-1 h-0.5 w-0 
                    bg-indigo-500 transition-all duration-300 
                    group-hover:w-full"
-      ></span>
-    </li>
-  ))}
-</ul>
+              ></span>
+            </li>
+          )
+        )}
+      </ul>
 
       <div className="search-input flex items-center xs:gap-1 md:gap-2 lg:gap-3 xl:gap-4">
         <Input
