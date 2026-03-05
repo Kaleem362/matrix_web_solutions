f10c1edf16a617761eaf6a983a342f927b404e8c
kaleem362 <matrixdev19@gmail.com>
2026-03-05 10:07:02 +0500

    Vercel analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import App from "./App";
 import "./index.css";
 import { ContextProvider } from "./Context/store";
 import { useStore } from "./Context/UseStore";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <ContextProvider store={useStore}>
     <App />
+    <Analytics />
   </ContextProvider>
 );
